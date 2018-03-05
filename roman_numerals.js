99dc118171a10c857595a0b08c4a2770dccd561f
Fitrul Islam <fitrul.islam@gmail.com>
2018-03-05 17:53:18 +0700

    ada stoknya

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,18 +1,40 @@
-function convertToCoin (money) {
-  var num = [10000,5000,2000,1000,500,200,100,50,20,10,1];
+function convertToCoin (input) {
+  var num = [{
+    money:10000,stock:2
+  },{
+    money:5000,stock:2
+  },{
+    money:2000,stock:2
+  },{
+    money:1000,stock:2
+  },{
+    money:500,stock:5
+  },{
+    money:200,stock:7
+  },{
+    money:100,stock:10
+  },{
+    money:50,stock:5
+  },{
+    money:20,stock:0
+  },{
+    money:10,stock:10
+  },{
+    money:1,stock:10
+  }];
   var result = [];
   for(let i=0;i<num.length;i++){
-    if(money >= num[i]){
-      money -= num[i];
-      result.push(num[i]);
-      i--;
+    while(num[i].stock>0 && input>=num[i].money){
+      input-=num[i].money;
+      result.push(num[i].money);
+      num[i].stock--;
     }
   }
   return result;
 }
 
 // Drive code
-console.log(convertToCoin(543))
+console.log(convertToCoin(99))
 //output : [ 500, 20, 20, 1, 1, 1 ]
 
 console.log(convertToCoin(7752))
