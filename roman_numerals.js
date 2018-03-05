4cdfe03c93cc6bcb94ad2e2d320febaed659dacd
annisaayu <annisaayu569@gmail.com>
2018-03-05 18:22:28 +0700

    money coin

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,14 @@
 function convertToCoin (money) {
   // your implementation code here
+  let counter = [1, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000];
+  let arrResult = [];
+  for(let i= counter.length -1; i >= 0; i--){
+    while(money >= counter[i]){
+      money-=counter[i];
+      arrResult.push(counter[i])
+    }
+  }
+  return arrResult;
 }
 
 // Drive code
