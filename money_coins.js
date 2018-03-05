e4a048caf27cc4f224ec05f4a67fdd62d065d92b
ekifakhrureza <ekifakhrureza@gmail.com>
2018-03-05 19:50:14 +0700

    COINS W/ QTY

--- money_coins.js
+++ money_coins.js
@@ -4,22 +4,29 @@ function money_coins(input){
           {nominal : 10, qty : 2},
           {nominal : 20, qty : 2},
           {nominal : 50, qty : 3},
-          {nominal : 100, qty : 2},
+          {nominal : 100, qty : 4},
         ]
   var arrHasil=[]
   var jml=0
   for (var i = coins.length-1; i >= 0 ; i--) {
-    while(input-coins[i].nominal>=0 && coins[i].qty>=0){
+    while(input-coins[i].nominal>=0 && coins[i].qty>0){
       input=input-coins[i].nominal
       coins[i].qty--
       jml++
     }
     if(jml!==0){
-      arrHasil.push('nominal : '+coins[i].nominal+', qty : '+jml)
+      var obj={nominal:coins[i].nominal,qty : jml}
+      arrHasil.push(obj)
     }
     jml=0
   }
-  console.log(arrHasil.join('\n'));
+
+  for (var i = 0; i < arrHasil.length; i++) {
+    for (var j = 0; j < arrHasil[i].qty; j++) {
+      console.log(arrHasil[i].nominal);
+    }
+  }
+
 }
 
-money_coins(152)
+money_coins(502)
