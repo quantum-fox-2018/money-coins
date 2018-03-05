a8828ea5054af4de75281b97ff5aa94561e84059
Dani Damara <danny12march@gmail.com>
2018-03-05 16:15:10 +0700

    Tugas Money Coins p1-w1-d1

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,62 @@
 function convertToCoin (money) {
-  // your implementation code here
+  let arrHasil = [];
+
+  while(money >= 10000){
+      arrHasil.push(10000);
+      money -= 10000;
+  }
+
+  while(money >= 5000){
+      arrHasil.push(5000);
+      money -= 5000;
+  }
+
+  while(money >= 2000){
+      arrHasil.push(2000);
+      money -= 2000;
+  }
+
+  while(money >= 1000){
+      arrHasil.push(1000);
+      money -= 1000;
+  }
+
+  while(money >= 500){
+      arrHasil.push(500);
+      money -= 500;
+  }
+
+  while(money >= 200){
+      arrHasil.push(200);
+      money -= 200;
+  }
+
+  while(money >= 100){
+      arrHasil.push(100);
+      money -= 100;
+  }
+
+  while(money >= 50){
+      arrHasil.push(50);
+      money -= 50;
+  }
+
+  while(money >= 20){
+      arrHasil.push(20);
+      money -= 20;
+  }
+
+  while(money >= 10){
+      arrHasil.push(10);
+      money -= 10;
+  }
+
+  while(money >= 1){
+      arrHasil.push(1);
+      money -= 1;
+  }
+
+  return arrHasil;
 }
 
 // Drive code
