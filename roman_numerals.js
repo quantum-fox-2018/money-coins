52ebf2a07762e076c0c904489e35bd98f95a37d5
OkyWiliarso <okywiliarso@gmail.com>
2018-03-05 17:50:33 +0700

    add tugas money coins

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,53 @@
 function convertToCoin (money) {
   // your implementation code here
+var arr = [];
+
+var tampung = money;
+  while (tampung >= 10000){
+    tampung = tampung - 10000;
+    arr.push(10000);
+  }
+  while (tampung >= 5000){
+    tampung = tampung - 5000;
+    arr.push(5000);
+  }
+  while (tampung >= 2000){
+    tampung = tampung - 2000;
+    arr.push(2000);
+  }
+  while (tampung >= 1000){
+    tampung = tampung - 1000;
+    arr.push(1000);
+  }
+  while (tampung >= 500){
+    tampung = tampung - 500;
+    arr.push(500);
+  }
+  while (tampung >= 200){
+    tampung = tampung - 200;
+    arr.push(200);
+  }
+  while (tampung >= 100){
+    tampung = tampung - 100;
+    arr.push(100);
+  }
+  while (tampung >= 50){
+    tampung = tampung - 50;
+    arr.push(50);
+  }
+  while (tampung >= 20){
+    tampung = tampung - 20;
+    arr.push(20);
+  }
+  while (tampung >= 10){
+    tampung = tampung - 10;
+    arr.push(10);
+  }
+  while (tampung >= 1){
+    tampung = tampung - 1;
+    arr.push(1);
+  }
+  return arr;
 }
 
 // Drive code
