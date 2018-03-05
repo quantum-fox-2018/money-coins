079e1257877056f05a411ba951610be40c92e8c5
Rafie Gilang <gilang0293@gmail.com>
2018-03-05 15:44:59 +0700

    selesai tugas money-coins

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,20 @@
 function convertToCoin (money) {
-  // your implementation code here
+  var result = [];
+  while (money > 0) {
+    if (money >= 10000) {money -= 10000; result.push(10000)}
+    else if (money >= 5000) {money -= 5000; result.push(5000)}
+    else if (money >= 2000) {money -= 2000; result.push(2000)}
+    else if (money >= 1000) {money -= 1000; result.push(1000)}
+    else if (money >= 500) {money -= 500; result.push(500)}
+    else if (money >= 200) {money -= 200; result.push(200)}
+    else if (money >= 100) {money -= 100; result.push(100)}
+    else if (money >= 50) {money -= 50; result.push(50)}
+    else if (money >= 20) {money -= 20; result.push(20)}
+    else if (money >= 10) {money -= 10; result.push(10)}
+    else {money -= 1; result.push(1)}
+  }
+
+  return result;
 }
 
 // Drive code
