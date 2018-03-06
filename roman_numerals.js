d85175d3b8d6d4f4205693243d1a550d25bf2e8b
NeilTanado <neiltanado@yahoo.co.id>
2018-03-06 14:09:13 +0700

    first push

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,14 @@
-function convertToCoin (money) {
-  // your implementation code here
+function convertToCoin (num) {
+  var koin = [10000,5000,2000,1000,500,200,100,50,20,10,1];
+  var hasil = [];
+
+  for(var i = 0 ; i < koin.length ; i++){
+    while(num >= koin[i]){
+      hasil.push(koin[i]);
+      num -= koin[i];
+    }
+  }
+  return hasil;
 }
 
 // Drive code
